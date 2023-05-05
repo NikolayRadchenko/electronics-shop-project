a6eb09c38ebae6392c90ce1336ae1b4c05978720
NikolayRadchenko <lenovnikolas@gmail.com>
2023-05-05 16:20:07 +0300

    HW_2 ready

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +16,31 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
         self.price = price
+        self.__name = name
         self.quantity = quantity
         self.all.append(self)
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('../src/items.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                Item(row['name'], float(row['price']), int(row['quantity']))
+
+    @staticmethod
+    def string_to_number(string):
+        return int(float(string))
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) > 10:
+            raise Exception("Длина наименования товара превышает 10 символов.")
+        self.__name = name
 
     def calculate_total_price(self) -> float:
         """
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,14 +1,37 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
-
-item1 = Item('Булочка', 2.0, 8)
-
-
-def test_calculate_total_price():
-    assert item1.calculate_total_price() == 16.0
+import pytest
 
 
-def test_apply_discount():
-    item1.apply_discount()
-    assert 2.0 * Item.pay_rate == item1.price
-    assert item1.price == 3.0
+@pytest.fixture
+def item_fixture():
+    return Item('Булочка', 2.0, 8)
+
+
+def test_calculate_total_price(item_fixture):
+    assert item_fixture.calculate_total_price() == 16.0
+
+
+def test_apply_discount(item_fixture):
+    item_fixture.apply_discount()
+    assert 2.0 * Item.pay_rate == item_fixture.price
+    assert item_fixture.price == 3.0
+
+
+def test_instantiate_from_csv(item_fixture):
+    item_fixture.instantiate_from_csv()
+    assert len(item_fixture.all) == 8
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5.5') == 5
+
+
+def test_name(item_fixture):
+    item_fixture.name = 'Смартфон'
+    assert item_fixture.name == 'Смартфон'
+
+
+def test_name__exception(item_fixture):
+    with pytest.raises(Exception):
+        item_fixture.name = 'СуперСмартфон'
