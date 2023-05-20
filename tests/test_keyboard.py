5794a783a1095c50dfcff3e25c89258927b50a70
NikolayRadchenko <lenovnikolas@gmail.com>
2023-05-20 20:44:39 +0300

    HW_5 ready

---
+++ tests/test_keyboard.py
@@ -0,0 +1,30 @@
+from src.keyboard import KeyBoard
+import pytest
+
+
+@pytest.fixture
+def keyboard_fixture():
+    return KeyBoard('Dark Project KD87A', 9600, 5)
+
+
+def test_str(keyboard_fixture):
+    assert str(keyboard_fixture) == "Dark Project KD87A"
+
+
+def test_language(keyboard_fixture):
+    assert str(keyboard_fixture.language) == "EN"
+
+
+def test_change_lange(keyboard_fixture):
+    keyboard_fixture.change_lang()
+    assert str(keyboard_fixture.language) == "RU"
+
+
+def test_mixin(keyboard_fixture):
+    keyboard_fixture.change_lang().change_lang()
+    assert str(keyboard_fixture.language) == "EN"
+
+
+def test_change_lange__attribute_error(keyboard_fixture):
+    with pytest.raises(AttributeError):
+        keyboard_fixture.language = 'CH'
