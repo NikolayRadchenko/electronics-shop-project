323331dd3c39dd3d8b042b355db1dc70b82f5942
NikolayRadchenko <lenovnikolas@gmail.com>
2023-05-20 20:04:32 +0300

    HW_5 ready

---
+++ src/keyboard.py
@@ -0,0 +1,27 @@
+from src.item import Item
+
+
+class MixinLanguage:
+
+    def change_lang(self):
+        if self.language == 'EN':
+            self.language = 'RU'
+            return self
+        self.language = 'EN'
+        return self
+
+
+class KeyBoard(Item, MixinLanguage):
+    def __init__(self, name: str, price: float, quantity: int, language: int = 'EN'):
+        super().__init__(name, price, quantity)
+        self.__language = language
+
+    @property
+    def language(self):
+        return self.__language
+
+    @language.setter
+    def language(self, new_language):
+        if new_language not in ['EN', 'RU']:
+            raise AttributeError("property 'language' of 'KeyBoard' object has no setter")
+        self.__language = new_language.upper()
